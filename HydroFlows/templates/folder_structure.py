b9389971799f4e3f7c6061dad9d96c9daaeef276
Dirk Eilander <dirk.eilander@gmail.com>
2024-02-27 12:19:46 +0100

    create package structure

--- HydroFlows/templates/folder_structure.py
+++ HydroFlows/templates/folder_structure.py
@@ -1,4 +1,56 @@
+# TODO move this into a cookiecutter template?
 # default nested folder structures for use in HydroFlows
+
+""" 
+├── .gitignore
+├── environment.yml
+├── README.md
+├── LICENSE.md
+├── workflow
+│   ├── rules
+|   │   ├── module1.smk
+|   │   └── module2.smk
+│   ├── envs
+|   │   ├── tool1.yaml
+|   │   └── tool2.yaml
+│   ├── scripts
+|   │   ├── script1.py
+|   │   └── script2.R
+│   ├── notebooks
+|   │   ├── notebook1.py.ipynb
+|   │   └── notebook2.r.ipynb
+|   └── Snakefile
+├── config
+│   ├── config.yaml
+│   └── some-sheet.tsv
+├── data
+│   ├── input
+│   └── output
+├── models
+│   ├── wflow
+|   │   └── {region1}
+|   │       ├── staticmaps.nc
+|   │       ├── wflow_sbm_default.toml 
+|   │       ├── hydromt_wflow.yaml
+|   │       └── simulations
+|   │           └── {sim1}
+|   │               ├── wflow_sim1.toml
+|   │               └── forcing.nc
+│   ├── sfincs
+|   │   └── {region1}
+|   │       ├── sfincs.xxx
+|   │       ├── sfincs.inp
+|   │       └── simulations
+|   │           └── {sim1}
+|   │               ├── sfincs.xxx
+|   │               └── sfincs.inp
+│   └── fiat
+├── results
+└── resources
+
+"""
+
+
 import os
 from typing import Union
 
