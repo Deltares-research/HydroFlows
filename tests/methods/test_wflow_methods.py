551eaa79930ed7689aa96cf050914a2818e06ada
Athanasios Tsiokanos <134158167+atsiokanos@users.noreply.github.com>
2024-04-03 18:29:54 +0200

    wflow build update method plus some code cleaning (#64)
    Co-authored-by: Dirk Eilander <dirk.eilander@gmail.com>

--- tests/methods/test_wflow_methods.py
+++ tests/methods/test_wflow_methods.py
@@ -1,38 +1,9 @@
-import os
 from pathlib import Path
 
-import geopandas as gpd
 import pytest
 from hydromt_wflow import WflowModel
-from shapely.geometry import Point
 
 from hydroflows.methods import WflowBuild, WflowUpdateForcing
-
-
-@pytest.fixture()
-def sfincs_src_points():
-    return gpd.GeoDataFrame(
-        geometry=[
-            Point(282937.059, 5079303.114),
-        ],
-        crs="EPSG:32633",
-    )
-
-
-def test_wflow_build(sfincs_src_points, tmp_path):
-    # write region to file
-    fn_sfincs_src_points = Path(tmp_path, "data", "sfincs_src_points.geojson")
-    os.makedirs(fn_sfincs_src_points.parent, exist_ok=True)
-    sfincs_src_points.to_file(fn_sfincs_src_points, driver="GeoJSON")
-
-    input = {"sfincs_src_points": str(fn_sfincs_src_points)}
-
-    fn_wflow_toml = Path(tmp_path, "model", "wflow.toml")
-    output = {"wflow_toml": str(fn_wflow_toml)}
-
-    WflowBuild(input=input, output=output).run()
-
-    assert fn_wflow_toml.exists()
 
 
 @pytest.fixture()
@@ -63,6 +34,26 @@ def wflow_simple_root(tmp_path):
     mod.write_config()
 
     return root
+
+
+
+def test_wflow_build(sfincs_src_points, sfincs_region_path, tmp_path):
+    # write src points to file
+    fn_sfincs_src_points = Path(sfincs_region_path.parent, "src.geojson")
+    sfincs_src_points.to_file(fn_sfincs_src_points, driver="GeoJSON")
+
+    input = {"sfincs_region": str(sfincs_region_path)}
+
+    fn_wflow_toml = Path(tmp_path, "model", "wflow.toml")
+    output = {"wflow_toml": str(fn_wflow_toml)}
+
+    WflowBuild(input=input, output=output).run()
+
+    fn_geoms = Path(fn_wflow_toml.parent, "staticgeoms", "gauges_locs.geojson")
+
+    assert fn_wflow_toml.exists()
+
+    assert fn_geoms.exists()
 
 
 def test_wflow_update_forcing(wflow_simple_root):
