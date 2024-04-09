c36752cab1b92dbf9f73688dea4e40e65733f74d
WOTrompDeltares <willem.tromp@deltares.nl>
2024-04-09 17:02:47 +0200

    Create sfincs_update test

--- tests/methods/test_sfincs_methods.py
+++ tests/methods/test_sfincs_methods.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from hydroflows.methods import SfincsBuild
+from hydroflows.methods import SfincsBuild, SfincsUpdateForcing
 
 
 def test_sfincs_build(sfincs_region_path , tmp_path):
@@ -20,3 +20,17 @@ def test_sfincs_build(sfincs_region_path , tmp_path):
     SfincsBuild(input=input, output=output, params=params).run()
 
     assert fn_sfincs_inp.exists()
+
+def test_sfincs_update(sfincs_test_root):
+    input = {"sfincs_inp": f"{sfincs_test_root}/sfincs.inp"}
+
+    fn_sfincs_event_inp = Path(sfincs_test_root, "scenario", "event", "sfincs.inp")
+    output = {"sfincs_inp": str(fn_sfincs_event_inp)}
+    params = {
+        "event_file": "",
+        "event_name": ""
+    }
+
+    SfincsUpdateForcing(input=input, output=output, params=params).run()
+
+    assert fn_sfincs_event_inp.exists()
