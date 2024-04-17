b96a823181731b71e6aa1af24199aefce8601612
Dirk Eilander <dirk.eilander@gmail.com>
2024-04-17 15:37:27 +0200

    better sources; add plot option

--- hydroflows/methods/sfincs/sfincs_build.py
+++ hydroflows/methods/sfincs/sfincs_build.py
@@ -1,7 +1,8 @@
 """SFINCS build methods."""
 from pathlib import Path
 
-from hydromt.config import configread
+from hydromt.config import configread, configwrite
+from hydromt.log import setuplog
 from hydromt_sfincs import SfincsModel
 from pydantic import BaseModel, FilePath
 
@@ -15,13 +16,15 @@ class Input(BaseModel):
 
 class Output(BaseModel):
     sfincs_inp: Path
+    sfincs_region: Path
 
 class Params(BaseModel):
     # optional parameter
     data_libs: ListOfStr = ['artifact_data']
     config: Path = Path(HYDROMT_CONFIG_DIR, "sfincs_build.yaml")
     res: float = 50.0
-
+    river_upa: float = 30
+    plot_fig: bool = True
 
 class SfincsBuild(Method):
     """Rule for building Sfincs."""
@@ -36,20 +39,35 @@ class SfincsBuild(Method):
         # read the configuration
         opt = configread(self.params.config)
         # update placeholders in the config
-        # TODO: look into templating options
         opt['setup_grid_from_region'].update(
             res = self.params.res,
             region = {'geom': str(self.input.region)}
         )
         opt['setup_mask_active'].update(
-            mask=self.input.region
+            mask=str(self.input.region)
+        )
+        opt['setup_river_inflow'].update(
+            river_upa=self.params.river_upa
         )
         # create the hydromt model
         root = self.output.sfincs_inp.parent
         sf = SfincsModel(
             root=root,
             mode='w+',
-            data_libs=self.params.data_libs
+            data_libs=self.params.data_libs,
+            logger=setuplog('sfincs_build', log_level=20),
         )
         # build the model
         sf.build(opt=opt)
+        sf.write_grid(['dep']) # make sure to also write the dep grid
+
+        # write the opt as yaml
+        configwrite(root / 'sfincs_build.yaml', opt)
+
+        # plot basemap
+        if self.params.plot_fig == True:
+            sf.plot_basemap(
+                fn_out='basemap.png',
+                plot_region=True,
+                shaded=True
+            )
