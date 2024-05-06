1e62e3d2379fa513191cd16144c44710f5239290
hcwinsemius <h.c.winsemius@gmail.com>
2024-05-06 19:23:44 +0200

    lint error #95

--- tests/methods/test_sfincs_methods.py
+++ tests/methods/test_sfincs_methods.py
@@ -8,22 +8,6 @@ import pytest
 from hydroflows.methods import SfincsBuild, SfincsPostprocess, SfincsUpdateForcing
 from hydroflows.methods.sfincs.sfincs_run import SfincsRun
 
-def test_sfincs_build(rio_region, rio_test_data, tmp_path):
-    input = {
-        "region": str(rio_region)
-    }
-
-    sfincs_inp = Path(tmp_path, "model", "sfincs.inp")
-    sfincs_region = Path(tmp_path, "model", "gis", "region.geojson")
-    output = {
-        "sfincs_inp": str(sfincs_inp),
-        "sfincs_region": str(sfincs_region)
-    }
-    params = {
-        "data_libs": str(rio_test_data),
-        "res": 100.0,
-        "river_upa": 10.0
-    }
 SFINCS_EXE = Path(__file__).parent.parent / "_bin" / "sfincs" / "sfincs.exe"
 
 def copy_tree(
@@ -45,12 +29,21 @@ def copy_tree(
 
 
 def test_sfincs_build(rio_region, rio_test_data, tmp_path):
-    input = {"region": str(rio_region)}
+    input = {
+        "region": str(rio_region)
+    }
 
     sfincs_inp = Path(tmp_path, "model", "sfincs.inp")
     sfincs_region = Path(tmp_path, "model", "gis", "region.geojson")
-    output = {"sfincs_inp": str(sfincs_inp), "sfincs_region": str(sfincs_region)}
-    params = {"data_libs": str(rio_test_data), "res": 100.0, "river_upa": 10.0}
+    output = {
+        "sfincs_inp": str(sfincs_inp),
+        "sfincs_region": str(sfincs_region)
+    }
+    params = {
+        "data_libs": str(rio_test_data),
+        "res": 100.0,
+        "river_upa": 10.0
+    }
     SfincsBuild(input=input, output=output, params=params).run()
     assert sfincs_inp.exists()
     assert sfincs_region.exists()
