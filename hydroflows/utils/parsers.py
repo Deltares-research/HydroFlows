00a0c82a0aa5dc9b703f46163874d7a1d44154f0
Dirk Eilander <dirk.eilander@gmail.com>
2024-08-21 15:06:09 +0200

    Revert "add simple wildcard validation"
    This reverts commit 034a2c71f592395bfd4d443a39a092fe194f6410.

--- hydroflows/utils/parsers.py
+++ hydroflows/utils/parsers.py
@@ -1,7 +1,5 @@
 """Some parser utils to be used with pydantic validators."""
-
 import re
-from typing import List, Optional
 
 
 def str_to_list(v: str) -> list[str]:
@@ -16,27 +14,3 @@ def str_to_list(v: str) -> list[str]:
     vlist = [m.group(1) or m.group(2) or m.group(0) for m in re.finditer(regex, v)]
     # strip whitespace and quotes from values
     return [v.strip("'\" ") for v in vlist]
-
-
-def get_wildcards(s, known_wildcards: Optional[List[str]] = None) -> List[str]:
-    """Return a list of wildcards in the form of `{*}` from a string.
-
-    Parameters
-    ----------
-    s : str
-        The string to search for wildcards.
-    known_wildcards : List[str], optional
-        List of known wildcards, by default None
-    """
-    if known_wildcards is not None:
-        # Define the regex pattern to match known wildcards
-        pattern = r"\{" + "|".join(known_wildcards) + r"\}"
-    else:
-        # Define the regex pattern to match any wildcard "{*}"
-        pattern = r"\{.*?\}"
-
-    # Find all matches of the pattern in the string
-    matches = re.findall(pattern, str(s))
-
-    # Return list of matches with curly braces stripped
-    return [str(wc).strip("{}") for wc in matches]
