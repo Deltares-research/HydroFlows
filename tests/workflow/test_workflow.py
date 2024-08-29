428eeb993a73e60e47a179bf4dc513ef324fa415
Tjalling de Jong <tjalling.dejong@deltares.nl>
2024-08-29 16:02:55 +0200

    temp commit

--- tests/workflow/test_workflow.py
+++ tests/workflow/test_workflow.py
@@ -1,12 +1,12 @@
 import io
 import os
+import subprocess
 from pathlib import Path
 from typing import List, Union
 
 import pytest
 import yaml
 
-from hydroflows import __version__
 from hydroflows.workflow import (
     Parameters,
     Ref,
@@ -42,12 +42,14 @@ class MockExpandMethod(ExpandMethod):
         events: List[str],
         wildcard: str = "wildcard",
     ) -> None:
-        self.input = ExpandMethodInput(input_file=input_file)
-        self.params = ExpandMethodParams(root=root, events=events, wildcard=wildcard)
-        wc = "{" + wildcard + "}"
-        self.output = ExpandMethodOutput(
-            output_file=Path(self.params.root) / f"{wc}.yml",
-            output_file2=Path(self.params.root) / f"{wc}_2.yml",
+        self.input: ExpandMethodInput = ExpandMethodInput(input_file=input_file)
+        self.params: ExpandMethodParams = ExpandMethodParams(
+            root=root, events=events, wildcard=wildcard
+        )
+        wc = "{" + self.params.wildcard + "}"
+        self.output: ExpandMethodOutput = ExpandMethodOutput(
+            output_file=self.params.root / f"{wc}.yml",
+            output_file2=self.params.root / f"{wc}_2.yml",
         )
         self.set_expand_wildcard(wildcard, self.params.events)
 
@@ -57,9 +59,9 @@ class MockExpandMethod(ExpandMethod):
             event_file = Path(str(self.output.output_file).format(**fmt_dict))
             test_data = {event: "test"}
             with open(event_file, "w") as f:
-                yaml.dumps(test_data, f)
+                yaml.dump(test_data, f)
         with open(self.output.output_file2, "w") as f:
-            yaml.dumps({"test_file": "2nd_test_file"}, f)
+            yaml.dump({"test_file": "2nd_test_file"}, f)
 
 
 class ReduceInput(Parameters):
@@ -79,24 +81,25 @@ class MockReduceMethod(ReduceMethod):
     name: str = "mock_reduce_method"
 
     def __init__(self, first_file: Path, second_file: Path, root: Path) -> None:
-        self.input = ReduceInput(first_file=first_file, second_file=second_file)
-        self.params = ReduceParams(root=root)
-        self.output = ReduceOutput(
-            output_file=Path(str(self.params.root) + "/output_file.yml")
+        self.input: ReduceInput = ReduceInput(
+            first_file=first_file, second_file=second_file
+        )
+        self.params: ReduceParams = ReduceParams(root=root)
+        self.output: ReduceOutput = ReduceOutput(
+            output_file=self.params.root / "output_file.yml"
         )
 
     def run(self):
         data = {
             "input1": self.input.first_file,
             "input2": self.input.second_file,
-            "name": self.params.name,
         }
         with open(self.output.output_file, "w") as f:
-            yaml.dumps(data, f)
+            yaml.dump(data, f)
 
 
 @pytest.fixture()
-def w():
+def w() -> Workflow:
     config = {"rps": [2, 50, 100]}
     wildcards = {"region": ["region1", "region2"]}
     return Workflow(name="wf_instance", config=config, wildcards=wildcards)
@@ -137,26 +140,34 @@ def mock_expand_method():
 
 
 def create_workflow_with_mock_methods(w, root="", input_file="test.yml"):
+    # create initial input file for workflow
+    if isinstance(root, Path):
+        with open(root / input_file, "w") as f:
+            yaml.dump(dict(test="test"), f)
+
     mock_expand_method = MockExpandMethod(
-        input_file=input_file, root=root, events=["1", "2"]
+        input_file=root / "{region}" / input_file,
+        root=root,
+        events=["1", "2"],
+        wildcard="region",
     )
     w.add_rule(method=mock_expand_method, rule_id="mock_expand_rule")
     mock_reduce_method = MockReduceMethod(
         first_file=w.get_ref("$rules.mock_expand_rule.output.output_file"),
         second_file=w.get_ref("$rules.mock_expand_rule.output.output_file2"),
-        root=root,
+        root=root / "{region}",
     )
     w.add_rule(method=mock_reduce_method, rule_id="mock_reduce_rule")
     return w
 
 
-def test_workflow_init(w):
+def test_workflow_init(w: Workflow):
     assert isinstance(w.config, WorkflowConfig)
     assert isinstance(w.wildcards.wildcards, dict)
     assert w.name == "wf_instance"
 
 
-def test_workflow_repr(w, mock_expand_method):
+def test_workflow_repr(w: Workflow, mock_expand_method):
     w.add_rule(method=mock_expand_method, rule_id="mock_expand_rule")
     repr_str = w.__repr__()
     assert "region1" in repr_str
@@ -164,7 +175,7 @@ def test_workflow_repr(w, mock_expand_method):
     assert "mock_expand_rule" in repr_str
 
 
-def test_workflow_add_rule(w):
+def test_workflow_add_rule(w: Workflow):
     w = create_workflow_with_mock_methods(w)
     assert len(w.rules) == 2
     assert isinstance(w.rules[0], Rule)
@@ -172,7 +183,7 @@ def test_workflow_add_rule(w):
     assert w.rules[1].rule_id == "mock_reduce_rule"
 
 
-def test_workflow_rule_from_kwargs(w, mocker, mock_expand_method):
+def test_workflow_rule_from_kwargs(w: Workflow, mocker, mock_expand_method):
     mocked_Method = mocker.patch("hydroflows.workflow.Method.from_kwargs")
     mocked_Method.return_value = mock_expand_method
     kwargs = {"rps": "$config.rps"}
@@ -182,8 +193,8 @@ def test_workflow_rule_from_kwargs(w, mocker, mock_expand_method):
     assert w.rules[0].rule_id == "mock_rule"
 
 
-def test_workflow_get_ref(w):
-    w = create_workflow_with_mock_methods(w)
+def test_workflow_get_ref(w: Workflow, tmp_path):
+    w = create_workflow_with_mock_methods(w, root=tmp_path)
     ref = w.get_ref("$config.rps")
     assert isinstance(ref, Ref)
     assert ref.value == w.config.rps
@@ -192,8 +203,8 @@ def test_workflow_get_ref(w):
     assert ref.value == Path("{wildcard}.yml")
 
 
-def test_workflow_from_yaml(tmpdir, workflow_yaml_dict):
-    test_yml = os.path.join(tmpdir, "test.yml")
+def test_workflow_from_yaml(tmp_path, workflow_yaml_dict):
+    test_yml = tmp_path / "test.yml"
     with open(test_yml, "w") as f:
         yaml.dump(workflow_yaml_dict, f, sort_keys=False)
 
@@ -211,7 +222,7 @@ def test_workflow_from_yaml(tmpdir, workflow_yaml_dict):
             "method",
         ],
     }
-    test_file = os.path.join(tmpdir, "test.yml")
+    test_file = tmp_path / "test.yml"
     with open(test_file, "w") as f:
         yaml.dump(test_yml, f, sort_keys=False)
 
@@ -227,31 +238,24 @@ def test_workflow_from_yaml(tmpdir, workflow_yaml_dict):
         Workflow.from_yaml(test_file)
 
 
-def test_workflow_to_snakemake(w, tmpdir):
-    test_file = os.path.join(tmpdir, "test.yml")
+def test_workflow_to_snakemake(w: Workflow, tmp_path):
+    test_file = tmp_path / "test.yml"
     with open(test_file, "w") as f:
         yaml.dump({"data": "test"}, f)
-    w = create_workflow_with_mock_methods(w, root=str(tmpdir), input_file=test_file)
-    snake_file = os.path.join(tmpdir, "snake_file.smk")
+    w = create_workflow_with_mock_methods(w, root=tmp_path, input_file=test_file)
+    snake_file = tmp_path / "snake_file.smk"
     w.to_snakemake(snakefile=snake_file)
-    assert "snake_file.config.yml" in os.listdir(tmpdir)
-    assert "snake_file.smk" in os.listdir(tmpdir)
-    with open(snake_file, "r") as f:
-        snake_string = f.read()
-    assert snake_string.startswith(
-        f"# This file was generated by hydroflows version {__version__}"
-    )
-    assert "mock_expand_rule" in snake_string
-    assert "mock_reduce_rule" in snake_string
-    assert "rule all" in snake_string
+    assert "snake_file.config.yml" in os.listdir(tmp_path)
+    assert "snake_file.smk" in os.listdir(tmp_path)
+    subprocess.run(["snakemake", "-s", str(snake_file), "--dry-run"]).check_returncode()
 
 
-def test_workflow_to_yaml(tmpdir, workflow_yaml_dict):
-    test_file = os.path.join(tmpdir, "test.yml")
+def test_workflow_to_yaml(tmp_path, workflow_yaml_dict):
+    test_file = tmp_path / "test.yml"
     with open(test_file, "w") as f:
         yaml.dump(workflow_yaml_dict, f, sort_keys=False)
     w = Workflow.from_yaml(test_file)
-    test_file2 = os.path.join(tmpdir, "test2.yml")
+    test_file2 = tmp_path / "test2.yml"
     w.to_yaml(test_file2)
     w2 = Workflow.from_yaml(test_file2)
     assert w.config == w2.config
@@ -264,11 +268,10 @@ def test_workflow_to_yaml(tmpdir, workflow_yaml_dict):
     )
 
 
-def test_workflow_run(mocker, w, tmpdir):
-    w = create_workflow_with_mock_methods(w, root=str(tmpdir))
+def test_workflow_run(mocker, w, tmp_path):
+    w = create_workflow_with_mock_methods(w, root=tmp_path)
     mock_stdout = mocker.patch("sys.stdout", new_callable=io.StringIO)
-    w.run(dryrun=True)
+    w.run(dryrun=True, missing_file_error=True)
     captured_stdout = mock_stdout.getvalue()
-    assert "Running dryrun in /tmp/hydroflows_" in captured_stdout
     for rule in w.rules:
         assert rule.rule_id in captured_stdout
