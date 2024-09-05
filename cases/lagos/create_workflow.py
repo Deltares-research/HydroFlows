47af4cae18da601460b4cc995fc421f18c96ad61
Brendan <brencodeert@outlook.com>
2024-09-05 14:18:09 +0200

    Add wflow to the mix

--- cases/lagos/create_workflow.py
+++ cases/lagos/create_workflow.py
@@ -1,6 +1,6 @@
 """Workflow file for the Lagos case."""
-
-# %% Import packages  # noqa: D100
+# %%
+# Import packages  # noqa: D100
 import os
 
 from hydroflows import Workflow
@@ -21,11 +21,17 @@ from hydroflows.methods.sfincs import (
 )
 from hydroflows.methods.wflow import (
     WflowBuild,
+    WflowDesignHydro,
+    WflowRun,
+    WflowUpdateForcing,
 )
 from hydroflows.workflow.workflow_config import WorkflowConfig
 
+# %%
 if __name__ == "__main__":
-    # %% Create a workflow config
+    pass
+    # %%
+    ## General setup of workflow
     setup_scenario_name = "global"  # for now
 
     conf = WorkflowConfig(
@@ -44,18 +50,21 @@ if __name__ == "__main__":
         river_upa=10,
         continent="Africa",
         risk=True,
-        start_date="1990-01-01",
+        start_date="2020-01-01",
         end_date="2023-12-31",
         plot_fig=True,
         depth_min=0.05,
+        wflow_exe="bin/wflow/bin/wflow_cli.exe",
         sfincs_exe="bin/sfincs/sfincs.exe",
         fiat_exe="bin/fiat/fiat.exe",
         # local_precip_path="preprocessed_data/output_scalar_resampled_precip_station11.nc",
     )
 
-    # %% Create a workflow
+    # Create a workflow
     w = Workflow(config=conf)
 
+    ## Build workflows
+    # Sfincs build
     sfincs_build = SfincsBuild(
         region=w.get_ref("$config.region"),
         sfincs_root=os.path.join(
@@ -71,7 +80,7 @@ if __name__ == "__main__":
     )
     w.add_rule(sfincs_build, rule_id="sfincs_build")
 
-    # %%
+    # Wflow build
     wflow_build = WflowBuild(
         region=sfincs_build.output.sfincs_region,
         wflow_root=os.path.join(
@@ -85,7 +94,9 @@ if __name__ == "__main__":
         plot_fig=w.get_ref("$config.plot_fig"),
     )
 
-    # %%
+    w.add_rule(wflow_build, rule_id="wflow_build")
+
+    # Fiat build
     fiat_build = FIATBuild(
         region=sfincs_build.output.sfincs_region,
         fiat_root=os.path.join(
@@ -100,7 +111,33 @@ if __name__ == "__main__":
 
     w.add_rule(fiat_build, rule_id="fiat_build")
 
-    # %%
+    ## Update and run wflow + generate fluvial events
+    # Update forcing
+    wflow_update = WflowUpdateForcing(
+        wflow_toml=wflow_build.output.wflow_toml,
+        start_time=w.get_ref("$config.start_date"),
+        end_time=w.get_ref("$config.end_date"),
+    )
+    w.add_rule(wflow_update, rule_id="wflow_update")
+
+    # Run wflow
+    wflow_run = WflowRun(
+        wflow_toml=wflow_update.output.wflow_out_toml,
+        wflow_bin=w.get_ref("$config.wflow_exe"),
+    )
+    w.add_rule(wflow_run, rule_id="wflow_run")
+
+    # Generate fluvial events
+    fluvial_events = WflowDesignHydro(
+        discharge_nc=wflow_run.output.wflow_output_timeseries,
+        rps=w.get_ref("$config.rps"),
+        wildcard="fluvial_event",
+        event_root="data/events",
+    )
+    w.add_rule(fluvial_events, rule_id="fluvial_events")
+
+    ## Pluvial events
+    # Get the data
     if setup_scenario_name == "global":
         get_precip = GetERA5Rainfall(
             region=sfincs_build.output.sfincs_region,
@@ -118,38 +155,37 @@ if __name__ == "__main__":
     else:
         precip_nc = w.get_ref("$config.local_precip_path")
 
-    # %%
+    # Actual derivation of events based on precip
     pluvial_events = PluvialDesignEvents(
         precip_nc=precip_nc,
-        event_root=os.path.join(
-            conf.setup_scenario,
-            conf.data_root_folder,
-            conf.sim_subfolder,
-            "events",
-            "rainfall",
-        ),
         rps=w.get_ref("$config.rps"),
         wildcard="pluvial_event",
+        event_root="data/events",
     )
     w.add_rule(pluvial_events, rule_id="pluvial_events")
 
-    # %%
+    ## In between logic to combine fluvial and pluvial events into one set
+    all_events = w.wildcards.get("pluvial_event") + w.wildcards.get("fluvial_event")
+    w.wildcards.set("all_events", all_events)
+
+    ## Updating, running and postprocessing SFINCS model
+    # Sfincs update with precip
     sfincs_update = SfincsUpdateForcing(
         sfincs_inp=sfincs_build.output.sfincs_inp,
         sim_subfolder=w.get_ref("$config.sim_subfolder"),
-        event_yaml=pluvial_events.output.event_yaml,
+        event_yaml="data/events/{all_events}.yml",
     )
     w.add_rule(sfincs_update, rule_id="sfincs_update")
 
-    # %%
+    # Run SFINCS model
     sfincs_run = SfincsRun(
         sfincs_inp=sfincs_update.output.sfincs_out_inp,
         sfincs_exe=w.get_ref("$config.sfincs_exe"),
     )
     w.add_rule(sfincs_run, rule_id="sfincs_run")
 
-    # %%
-    sfincs_postprocess = SfincsPostprocess(
+    # Postprocesses SFINCS results
+    sfincs_post = SfincsPostprocess(
         sfincs_map=sfincs_run.output.sfincs_map,
         sfincs_subgrid_dep=sfincs_build.output.sfincs_subgrid_dep,
         depth_min=w.get_ref("$config.depth_min"),
@@ -160,27 +196,30 @@ if __name__ == "__main__":
             "output",
             "hazard",
         ),
+        event_name="{all_events}",
     )
+    w.add_rule(sfincs_post, rule_id="sfincs_post")
 
-    w.add_rule(sfincs_postprocess, rule_id="sfincs_postprocess")
+    ## Update and run FIAT
+    # Set the combined event_set of both fluvial and pluvial
+    w.wildcards.set("event_set", ["fluvial_events", "pluvial_events"])
 
-    # %%
-    fiat_update_hazard = FIATUpdateHazard(
+    # Update hazard
+    fiat_update = FIATUpdateHazard(
         fiat_cfg=fiat_build.output.fiat_cfg,
-        event_set_yaml=pluvial_events.output.event_set_yaml,
+        event_set_yaml="data/events/{event_set}.yml",
+        event_set_name="{event_set}",
         sim_subfolder=w.get_ref("$config.sim_subfolder"),
-        hazard_maps=sfincs_postprocess.output.hazard_tif,
+        hazard_maps=sfincs_post.output.hazard_tif,
         risk=w.get_ref("$config.risk"),
     )
+    w.add_rule(fiat_update, rule_id="fiat_update")
 
-    w.add_rule(fiat_update_hazard, rule_id="fiat_update_hazard")
-
-    # %%
+    # Run FIAT
     fiat_run = FIATRun(
-        fiat_cfg=fiat_update_hazard.output.fiat_out_cfg,
+        fiat_cfg=fiat_update.output.fiat_out_cfg,
         fiat_bin=w.get_ref("$config.fiat_exe"),
     )
-
     w.add_rule(fiat_run, rule_id="fiat_run")
 
     # %% Test the workflow
