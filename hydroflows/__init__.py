febdf6564a37581cff9ba1d67577c79677659bd6
DirkEilander <15379728+DirkEilander@users.noreply.github.com>
2024-09-11 17:51:33 +0200

    update events.py (#171)

--- hydroflows/__init__.py
+++ hydroflows/__init__.py
@@ -3,10 +3,12 @@
 __version__ = "0.1.0.dev"
 
 
-from hydroflows.workflow import Method, Workflow
+from hydroflows.workflow import Method, Wildcards, Workflow, WorkflowConfig
 
 __all__ = [
     "__version__",
-    "Workflow",
     "Method",
+    "Wildcards",
+    "Workflow",
+    "WorkflowConfig",
 ]
