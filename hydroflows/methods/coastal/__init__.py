dbc7c5dbfad64405cd22153e38272f6f566373d4
WOTrompDeltares <142423474+WOTrompDeltares@users.noreply.github.com>
2024-09-18 15:23:34 +0200

    Coastal events (#133)
    Co-authored-by: Dirk Eilander <dirk.eilander@gmail.com>
    Co-authored-by: Athanasios Tsiokanos <134158167+atsiokanos@users.noreply.github.com>
    Co-authored-by: Marnix <150045289+deltamarnix@users.noreply.github.com>
    Co-authored-by: hcwinsemius <h.c.winsemius@gmail.com>
    Co-authored-by: DirkEilander <15379728+DirkEilander@users.noreply.github.com>

---
+++ hydroflows/methods/coastal/__init__.py
@@ -0,0 +1,6 @@
+"""Coastal workflow methods submodule."""
+from .coastal_design_events import CoastalDesignEvents
+from .coastal_design_events_from_rp_data import CoastalDesignEventFromRPData
+from .coastal_tidal_analysis import CoastalTidalAnalysis
+from .get_coast_rp import GetCoastRP
+from .get_gtsm_data import GetGTSMData
