36f70677fe3b7ceea112e840562e59741155171f
Athanasios Tsiokanos <134158167+atsiokanos@users.noreply.github.com>
2024-10-03 10:38:40 +0200

    update fluvial design events (#199)

--- hydroflows/methods/wflow/__init__.py
+++ hydroflows/methods/wflow/__init__.py
@@ -1,8 +1,7 @@
 """Wflow methods submodule."""
 
 from hydroflows.methods.wflow.wflow_build import WflowBuild
-from hydroflows.methods.wflow.wflow_design_hydro import WflowDesignHydro
 from hydroflows.methods.wflow.wflow_run import WflowRun
 from hydroflows.methods.wflow.wflow_update_forcing import WflowUpdateForcing
 
-__all__ = ["WflowBuild", "WflowDesignHydro", "WflowRun", "WflowUpdateForcing"]
+__all__ = ["WflowBuild", "WflowRun", "WflowUpdateForcing"]
