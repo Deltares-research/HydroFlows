bf026dbceb3b8f1c23d9bcf02b582fb2bd9c3769
Santonia27 <sarah.rautenbach@deltares.nl>
2024-10-24 13:25:32 +0200

    add config files and model run scripts

---
+++ cases/Rio/scripts/run_hydromtfiat_rio.py
@@ -0,0 +1,27 @@
+# import required packages
+import os
+from hydromt_fiat.fiat import FiatModel
+from hydromt.log import setuplog
+from pathlib import Path
+import geopandas as gpd
+from hydromt.config import configread
+
+
+# Change the working directory to the script's directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
+# Build model von config file
+root = Path(os.path.abspath("")) / "fiat_model"  
+logger = setuplog("hydromt_fiat", log_level=10)
+opt =  configread(Path(os.path.abspath("")) / "configuration.yml") 
+
+region = gpd.read_file(Path(os.path.abspath("")) / "data"/ "region.gpkg")
+
+fm = FiatModel(
+    root=root, mode="w+", logger=logger
+)
+
+# Build the new model
+fm.build(region={"geom": region}, opt=opt,write=False)
+fm.write()
