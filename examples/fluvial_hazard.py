164c72f59f1c3209afdde4485b3cba3e9f3c7474
Tjalling de Jong <tjalling.dejong@deltares.nl>
2024-11-27 11:50:35 +0100

    fixed linting issue

--- examples/fluvial_hazard.py
+++ examples/fluvial_hazard.py
@@ -23,7 +23,6 @@ from hydroflows.workflow.workflow_config import WorkflowConfig
 if __name__ == "__main__":
     # Get current file location
     pwd = Path(__file__).parent
-    
     # Setup logging
     logger = setuplog(path="./fluvial_hazard.log")
 
