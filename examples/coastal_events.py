780adc16ca225c78ba96c14730d4805f9b214335
WOTrompDeltares <willem.tromp@deltares.nl>
2024-12-10 11:46:22 +0100

    expand coastal example to coastal hazard

--- examples/coastal_events.py
+++ examples/coastal_events.py
@@ -1,8 +1,15 @@
 """Generate coastal events example."""
 
 # %% Import modules
+import os
 from pathlib import Path
 
+from hydroflows.log import setuplog
+from hydroflows.methods.sfincs import (
+    SfincsBuild,
+    SfincsUpdateForcing,
+    SfincsRun,
+)
 from hydroflows.methods.coastal import (
     CoastalDesignEvents,
     GetGTSMData,
@@ -17,20 +24,53 @@ if __name__ == "__main__":
     cache_dir = fetch_data(data="global-data")
 
     # %% Setup variables
-    name = "coastal_events"
+    name = "coastal_hazard"
+    model_dir = "models"
     data_dir = "data"
     input_dir = "input"
+    output_dir = "output"
+    simu_dir = "simulations"
+
+    case_root = Path(pwd, "cases", name)
+    setuplog(path=case_root / "hydroflows.log", level="DEBUG")
+
+    # Create the case directory
+    case_root.mkdir(exist_ok=True, parents=True)
+    os.chdir(case_root)
 
     # Setup the configuration
     config = WorkflowConfig(
+        # General settings
         region=Path(pwd, "data/build/region.geojson"),
         gtsm_catalog=Path(cache_dir, "data_catalog.yml"),
+        data_libs=[Path(cache_dir, "data_catalog.yml")],
         start_time="2014-01-01",
         end_time="2021-12-31",
+        plot_fig=True,
+        # sfincs settings
+        hydromt_sfincs_config=Path(pwd, "hydromt_config/sfincs_config.yml"),
+        sfincs_exe=Path(pwd, "bin/sfincs_v2.1.1/sfincs.exe"),
+        sfincs_res=50,
+        river_upa=10,
+        # design events
         rps=[2, 5, 10],
     )
 
     w = Workflow(config=config)
+
+    # %%
+
+    sfincs_build = SfincsBuild(
+        region=w.get_ref("$config.region"),
+        sfincs_root=Path(model_dir,"sfincs"),
+        default_config=w.get_ref("$config.hydromt_sfincs_config"),
+        data_libs=w.get_ref("$config.data_libs"),
+        res=w.get_ref("$config.sfincs_res"),
+        river_upa=w.get_ref("$config.river_upa"),
+        plot_fig=w.get_ref("$config.plot_fig")
+    )
+
+    w.add_rule(sfincs_build,rule_id="sfincs_build")
 
     # %% Get the GTSM data
     get_gtsm_data = GetGTSMData(
@@ -51,6 +91,24 @@ if __name__ == "__main__":
     )
     w.add_rule(coastal_design_events, rule_id="coastal_design_events")
 
+    # %%
+
+    sfincs_update = SfincsUpdateForcing(
+        sfincs_inp=sfincs_build.output.sfincs_inp,
+        sim_subfolder=simu_dir,
+        event_yaml=coastal_design_events.output.event_yaml
+    )
+
+    w.add_rule(sfincs_update, rule_id="sfincs_update")
+
+    # %%
+    
+    sfincs_run = SfincsRun(
+        sfincs_inp=sfincs_update.output.sfincs_out_inp,
+        sfincs_exe=w.get_ref("$config.sfincs_exe")
+    )
+
+    w.add_rule(sfincs_run, rule_id="sfincs_run")
     # %% Test the workflow
     w.run(dryrun=True)
 
