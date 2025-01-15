dc680c68a55659d23749ac9160cac7ebdc1f3a65
DirkEilander <dirk.eilander@gmail.com>
2025-01-15 13:08:39 +0100

    add examples to doc build

---
+++ docs/run_notebooks.py
@@ -0,0 +1,18 @@
+"""Small script to run all notebooks in the current directory using nbcovert."""
+
+import os
+from pathlib import Path
+
+import papermill as pm
+
+
+
+if __name__ == "__main__":
+    current_dir = Path(__file__).parent
+    examples_dir = current_dir / "../examples"
+    output_dir = Path(current_dir, "_examples")
+    os.chdir(examples_dir)
+    for nb in Path(examples_dir).glob("*.ipynb"):
+        nb_name = nb.name
+        print(f"Running {nb_name}")
+        pm.execute_notebook(nb, output_path=output_dir / nb_name)
