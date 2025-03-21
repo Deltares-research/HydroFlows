0c39a56b4d18bfec8770a406bd70462ce1221ec9
Santonia27 <sarah.rautenbach@deltares.nl>
2025-03-21 08:03:49 +0100

    add sm method to create waterlevels

--- hydroflows/methods/flood_adapt/prep_sfincs_models.py
+++ hydroflows/methods/flood_adapt/prep_sfincs_models.py
@@ -2,7 +2,6 @@
 import shutil
 from pathlib import Path
 
-import numpy as np
 from hydromt_sfincs import SfincsModel
 
 from hydroflows.workflow.method import Method
@@ -12,7 +11,7 @@ __all__ = ["PrepSfincsModels"]
 
 
 class Input(Parameters):
-    """Input parameters for the :py:class:`SetupFloodAdapt` method."""
+    """Input parameters for the :py:class:`PrepSfincsModels` method."""
 
     sfincs_inp: Path
     """
@@ -21,14 +20,14 @@ class Input(Parameters):
 
 
 class Output(Parameters):
-    """Output parameters for the :py:class:`SetupFloodAdapt` method."""
+    """Output parameters for the :py:class:`PrepSfincsModels` method."""
 
     sfincs_out_inp: Path
     """The path to the copied sfincs model configuration."""
 
 
 class Params(Parameters):
-    """Parameters for the :py:class:`SetupFloodAdapt` method."""
+    """Parameters for the :py:class:`PrepSfincsModels` method."""
 
     output_dir: Path = Path("flood_adapt_builder")
     """
@@ -79,20 +78,34 @@ class PrepSfincsModels(Method):
     def _run(self):
         """Run the PrepSfincsModels method."""
         # Get all sfincs models and prepare and copy sfincs model
-        sfincs_model = Path(self.params.output_dir, self.input.sfincs_inp.parent)
+        sfincs_model = Path(self.params.output_dir, self.input.sfincs_inp.parent.stem)
         shutil.copytree(
             Path(self.input.sfincs_inp.parent.parent / self.input.sfincs_inp.parent),
             sfincs_model,
             dirs_exist_ok=True,
         )
-        if not Path(sfincs_model, "sfincs.bnd").exists():
-            sm = SfincsModel(
-                root=self.input.sfincs_inp.parent,
-                mode="r",
-            )
-            sm.setup_waterlevel_bnd_from_mask(np.inf)
+        sm = SfincsModel(
+            root=sfincs_model,
+            mode="r",
+        )
+
+        if "bndfile" not in sm.config:
+            sm.setup_waterlevel_bnd_from_mask(10000)
+            sm.write_forcing()
             sm.config.pop("bzsfile")
             Path(sfincs_model, "sfincs.bzs").unlink()
+
+        # Remove discharge
+        if "disfile" in sm.config:
+            Path(sfincs_model, "sfincs.dis").unlink()
+            sm.config.pop("disfile")
+            sm.write_config()
+
+        # Remove simulation and figure folder
+        if Path(sfincs_model, "simulations").exists():
+            shutil.rmtree(Path(sfincs_model, "simulations"))
+        if Path(sfincs_model, "figs").exists():
+            shutil.rmtree(Path(sfincs_model, "figs"))
 
         # Remove discharge
         if Path(sfincs_model, "sfincs.dis").exists():
